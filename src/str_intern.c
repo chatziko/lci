3777f368b31945659bf321acbb356c402f43273d
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-06-13 16:36:20 +0300

    file cleanup

--- src/str_intern.c
+++ src/str_intern.c
@@ -1,17 +1,5 @@
-/* Basic string interning utilities
-
-	Copyright (C) 2019 Stefanos Baziotis
-	This file is part of LCI
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details. */
+// Basic string interning utilities
+// Copyright Kostas Chatzikokolakis and Stefanos Baziotis
 
 #include <assert.h>
 #include <stdlib.h>
--- src/termproc.h
+++ src/termproc.h
@@ -1,17 +1,5 @@
-/* Declarations for termproc.c
-
-	Copyright (C) 2004-8 Kostas Chatzikokolakis
-	This file is part of LCI
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details. */
+// Declarations for termproc.c
+// Copyright Kostas Chatzikokolakis
 
 #pragma once
 
